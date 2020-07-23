d8e1669ddf59dbf97c24ff5c84cdc4c740c6c3c6
korosuke613 <korosuke613@users.noreply.github.com>
2020-07-23 06:58:14 +0000

    deploy: db74d0e7acb9f74d5f4bf8914a5e8f27b66b76c6

---
+++ precache-manifest.6827e58d14ed04f1a7b5fdd885e2fae3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33cfdeef751d1f8dcf3f1c78b1bed5f7",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "6b189f493f21e0c8427c",
+    "url": "/react-tutorial/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/react-tutorial/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b189f493f21e0c8427c",
+    "url": "/react-tutorial/static/js/main.4459d754.chunk.js"
+  },
+  {
+    "revision": "4448310a0658a63dc063",
+    "url": "/react-tutorial/static/js/runtime-main.47be6dc6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tutorial/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a631dae74babec8264dfd13bb3eca3d5.js"
+  "/react-tutorial/precache-manifest.6827e58d14ed04f1a7b5fdd885e2fae3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tutorial/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
